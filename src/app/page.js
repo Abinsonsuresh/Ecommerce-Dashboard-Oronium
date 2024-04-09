96d9f58b430363313ea373fb1e96b860e7547679
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 02:54:09 +0530

    mainpage added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
+import MainHome from "@/Pages/Home/Main/MainHome";
 export default function Home() {
   return (
-   <div>
-    
-   </div>
+    <div>
+      <div >
+        <MainHome />
+      </div>
+    </div>
   );
 }
