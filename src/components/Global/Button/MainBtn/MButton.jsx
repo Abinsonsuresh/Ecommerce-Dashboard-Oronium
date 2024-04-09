b060509bf76a4f541c693c9249362828156fed77
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 02:01:43 +0530

    Main Button layout created

--- src/components/Global/Button/MainBtn/MButton.jsx
+++ src/components/Global/Button/MainBtn/MButton.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
 
-const MButton = () => {
+const MButton = ({text,icon}) => {
   return (
-    <div>MButton</div>
+    <div>
+        <button className='flex items-center gap-2 w-full px-3 py-2 rounded-lg bg-[#C63E4A] '>
+            {icon}
+            <span className='text-[14px] font-[600] leading-[21px] text-white'>{text}</span>
+        </button>
+    </div>
   )
 }
 
