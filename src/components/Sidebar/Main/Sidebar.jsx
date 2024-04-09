97c0215b651f7857db16d1b77b6e9c72d7778bd8
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 03:37:17 +0530

    theme sec added & sidebar completed

--- src/components/Sidebar/Main/Sidebar.jsx
+++ src/components/Sidebar/Main/Sidebar.jsx
@@ -10,17 +10,17 @@ const Sidebar = () => {
     };
     return (
         <div>
-            <div className='flex w-[264px] border-r border-[#e7e5e5] p-4 pt-8'>
-                <div>
+            <div className='flex flex-col  w-[264px] border-r border-[#e7e5e5] p-4 pt-8'>
+                <div className='flex items-center w-full flex-col'>
                     {
                         SCData.map((com) => {
                             return (
-                                <div key={com.id} onClick={() => handleItemClick(com.id)} className={selectedItem === com.id ? 'selected cursor-pointer bg-[#FCF3F4] w-[216px] h-[60px]  rounded-[6px] py-[12px] pl-[12px] pr-[16px]' : 'w-[216px] h-[60px]  rounded-[6px] py-[12px] pl-[12px] pr-[16px] cursor-pointer'}>
-                                    <div className='flex gap-3 items-center'>
+                                <div key={com.id} onClick={() => handleItemClick(com.id)} className={selectedItem === com.id ? 'selected gap-[8px]  cursor-pointer w-full bg-[#FCF3F4]  h-[60px]  rounded-[6px] py-[12px] pl-[12px]' : 'w-[216px] h-[60px]  rounded-[6px] gap-[8px]  py-[12px] pl-[12px] pr-[16px] cursor-pointer'}>
+                                    <div className='flex gap-3 items-center '>
                                         <span className={selectedItem === com.id ? 'bg-[#e3a9ae] rounded-lg p-2' : ''}>
-                                        {com.icon}
+                                            {com.icon}
                                         </span>
-                                        <span>{com.item}</span>
+                                        <span className='text-[16px] leading-[24px]'>{com.item}</span>
                                     </div>
                                 </div>
                             )
@@ -28,6 +28,16 @@ const Sidebar = () => {
                     }
 
                 </div>
+                    <div className='flex flex-col justify-start mt-[84px] py-[12px] pl-[12px]'>
+                        <span>Theme</span>
+                        <div className='flex items-center mt-4 rounded-[8px] bg-[#eae9e9] p-[12px] justify-between'>
+                            <span className='text-[14px]'>Primary color</span>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="8" cy="8" r="8" fill="#C63E4A" />
+                            </svg>
+
+                        </div>
+                    </div>
 
             </div>
         </div >
