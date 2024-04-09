177b4ccd7bdf4a9b547eebe3714434f39283f058
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 03:20:00 +0530

    sidebar selected items active state added

--- src/components/Sidebar/Main/Sidebar.jsx
+++ src/components/Sidebar/Main/Sidebar.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { SCData } from '../Data/SidebarComponentsData'
 
 const Sidebar = () => {
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const handleItemClick = (id) => {
+        setSelectedItem(id);
+    };
     return (
         <div>
             <div className='flex w-[264px] border-r border-[#e7e5e5] p-4 pt-8'>
@@ -9,9 +15,13 @@ const Sidebar = () => {
                     {
                         SCData.map((com) => {
                             return (
-                                <div key={com.id} className='w-[216px] h-[60px] flex gap-3 items-center rounded-[6px] py-[12px] pl-[12px] pr-[16px]'>
-                                    {com.icon}
-                                    <span>{com.item}</span>
+                                <div key={com.id} onClick={() => handleItemClick(com.id)} className={selectedItem === com.id ? 'selected cursor-pointer bg-[#FCF3F4] w-[216px] h-[60px]  rounded-[6px] py-[12px] pl-[12px] pr-[16px]' : 'w-[216px] h-[60px]  rounded-[6px] py-[12px] pl-[12px] pr-[16px] cursor-pointer'}>
+                                    <div className='flex gap-3 items-center'>
+                                        <span className={selectedItem === com.id ? 'bg-[#e3a9ae] rounded-lg p-2' : ''}>
+                                        {com.icon}
+                                        </span>
+                                        <span>{com.item}</span>
+                                    </div>
                                 </div>
                             )
                         })
@@ -20,7 +30,7 @@ const Sidebar = () => {
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
