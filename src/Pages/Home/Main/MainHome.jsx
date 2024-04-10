f18c48cf8d864ff88ac958de23c9200670d046ea
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 13:53:21 +0530

    Header Section completed

--- src/Pages/Home/Main/MainHome.jsx
+++ src/Pages/Home/Main/MainHome.jsx
@@ -5,9 +5,21 @@ const MainHome = () => {
   return (
     <div>
       <div className='flex'>
-        <Sidebar/>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A architecto similique nam veritatis molestiae optio tempore ullam impedit suscipit voluptatem, neque mollitia quidem aliquam atque quis quo modi et asperiores soluta possimus? Eos placeat eaque deleniti perspiciatis voluptatem debitis, accusamus, vitae sequi sit consectetur labore odit dicta ipsum, quis magnam omnis cum. Modi maxime, voluptatum ea numquam, odio ipsam mollitia veniam fugit reprehenderit ab minus non placeat officiis repellat voluptatem! Explicabo, enim tempore. Quisquam consectetur deserunt soluta enim quibusdam totam at, iusto doloremque incidunt ad sequi provident fuga, rem libero nesciunt reiciendis maxime quae aut ipsum nobis! Quas accusantium repellat facilis, dignissimos delectus illum nisi quam placeat modi officiis, distinctio iusto fugit esse rerum voluptatem animi. Exercitationem deserunt delectus accusantium maxime rem cumque neque facere ipsum provident atque impedit molestias repudiandae quis hic aperiam nam ducimus voluptatem corrupti alias enim eveniet, temporibus, perferendis similique labore? Necessitatibus odit ad eligendi quis ex hic iste dicta quos iure ipsa! Hic optio reprehenderit sapiente fugit repellat minima exercitationem est eligendi dolorem natus quam voluptatibus alias, commodi dolore ex nobis ad quia vero ea! A iusto accusamus perferendis sequi facilis! Temporibus quia dolorum pariatur quo debitis quidem, enim, laudantium eius ipsa, veritatis ducimus qui?
+        <Sidebar />
+        <div className='p-6'>
+
+          <div>
+            <h2 className='my-4 text-[20px] font-[700] leading-[28px] '>Header</h2>
+
+            <div>
+              <span className='text-[16px] font-[600] leading-[28px] text-[#29384bb9]'>Store logo</span>
+              <div className='h-[118px] w-[472px] rounded-[8px] border-2 flex justify-center items-center  border-dashed mt-2'>
+                <p>H</p>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </div>
