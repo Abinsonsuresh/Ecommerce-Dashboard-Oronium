04e0d2370b8010d0a2fea06eeaa48a665042cdec
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-11 00:43:50 +0530

    icons changed

--- src/Pages/Home/Data/SocialMediaIcons.js
+++ src/Pages/Home/Data/SocialMediaIcons.js
@@ -1,8 +1,8 @@
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
-
+import { FaFacebook , FaInstagram } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
 export const SMIcons = [
-    { id: 1, icon: <FaInstagram />, name: 'Instagram' },
-    { id: 2, icon: <FaFacebookF />, name: 'Facebook'},
-    { id: 3, icon: <FaTwitter />, name: 'Twitter' },
+    { id: 1, icon: <FaInstagram size={20}/>, name: 'Instagram' },
+    { id: 2, icon: <FaFacebook size={20} />, name: 'Facebook'},
+    { id: 3, icon: <FaXTwitter  size={20} />, name: 'Twitter' },
 ];
 
