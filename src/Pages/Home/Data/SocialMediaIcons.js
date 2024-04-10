8e6e93d2f101ad8e827801376667d35d76c27e6c
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 14:51:17 +0530

    social media section added

---
+++ src/Pages/Home/Data/SocialMediaIcons.js
@@ -0,0 +1,8 @@
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+export const SMIcons = [
+    { id: 1, icon: <FaFacebookF />, name: 'Facebook'},
+    { id: 2, icon: <FaTwitter />, name: 'Twitter' },
+    { id: 3, icon: <FaInstagram />, name: 'Instagram' },
+];
+
