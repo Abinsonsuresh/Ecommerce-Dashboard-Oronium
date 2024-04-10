e763ca24b8cb96c1890f5cc0c91248fc1c183d26
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-10 15:04:11 +0530

    Dropdown component and icons mapped

--- src/Pages/Home/Main/MainHome.jsx
+++ src/Pages/Home/Main/MainHome.jsx
@@ -1,26 +1,9 @@
-'use client'
 import Sidebar from '@/components/Sidebar/Main/Sidebar'
 import { SMIcons } from '../Data/SocialMediaIcons';
-import { useEffect, useState } from 'react';
-import { FaInstagram } from 'react-icons/fa';
+import DropDownBtn from '@/components/Global/Dropdown/Main/DropDownBtn';
 
 const MainHome = () => {
-  const [selectedIcon, setSelectedIcon] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    setSelectedIcon(<FaInstagram />);
-  }, [])
-
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const SetIconFn = (icon) => {
-    setSelectedIcon(icon);
-    setIsOpen(false);
-  };
 
   return (
     <div>
@@ -56,37 +39,18 @@ const MainHome = () => {
 
           <div className='mt-4'>
             <span className='text-[16px] font-[600] leading-[28px] text-[#29384bb9]'>Social Media</span>
-            <div className='flex items-center gap-3 mt-2'>
-              <div>
-                <div className='relative'>
-                  <div onClick={toggleDropdown} className="flex items-center cursor-pointer  justify-between w-[76px] h-[44px] py-[12px] pl-[12px] pr-[16px] rounded-[8px]  border text-gray-700">
-                    <button id="dropdownSmallButton"  className="inline-flex items-center text-center  focus:outline-none " type="button">{selectedIcon}
-                    </button>
-                    <svg className="w-2 h-2 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                      <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                    </svg>
-                  </div>
+            <div >
+              {
+                SMIcons.map((com) => {
+                  return (
+                    <div className='flex items-center gap-3 mt-2'>
+                      <DropDownBtn com={com} />
+                      <input type="text" className='w-[388px] h-[44px] py-[11px] px-[16px] border-[1px] outline-none rounded-[8px]' placeholder='martin.store' />
+                    </div>
+                  )
+                })
+              }
 
-
-                  {
-                    isOpen && (
-                      <div id="dropdownSmall" className="z-10 absolute my-2   divide-gray-100 rounded-lg shadow w-[70px]">
-                        <ul className="py-2 text-sm text-gray-700 ">
-                          {
-                            SMIcons.map((com) => (
-                              <li onClick={() => SetIconFn(com.icon)} className='block px-4 py-2' key={com.id}>
-                                {com.icon}
-                              </li>
-                            ))
-                          }
-
-                        </ul>
-                      </div>
-                    )
-                  }
-                </div>
-              </div>
-              <input type="text" className='w-[388px] h-[44px] py-[11px] px-[16px] border-[1px] outline-none rounded-[8px]' placeholder='martin.store' />
             </div>
           </div>
 
---
+++ src/components/Global/Dropdown/Main/DropDownBtn.jsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { SMIcons } from '@/Pages/Home/Data/SocialMediaIcons';
+import { useEffect, useState } from 'react';
+
+
+const DropDownBtn = ({ com }) => {
+
+    const [selectedIcon, setSelectedIcon] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        setSelectedIcon(com.icon);
+    }, [])
+
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const SetIconFn = (icon) => {
+        setSelectedIcon(icon);
+        setIsOpen(false);
+    };
+    return (
+        <div>
+            <div className='relative'>
+                <div onClick={toggleDropdown} className="flex items-center cursor-pointer  justify-between w-[76px] h-[44px] py-[12px] pl-[12px] pr-[16px] rounded-[8px]  border text-gray-700">
+                    <button id="dropdownSmallButton" className="inline-flex items-center text-center  focus:outline-none " type="button">{selectedIcon}
+                    </button>
+                    <svg className="w-2 h-2 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                    </svg>
+                </div>
+
+
+                {
+                    isOpen && (
+                        <div id="dropdownSmall" className="z-50 absolute my-2 h-full rounded-lg shadow w-[70px]">
+                            <ul className="py-2 text-sm bg-[#ffffff] border w-[76px] shadow-sm flex flex-col items-center rounded-[8px]">
+                                {
+                                    SMIcons.map((comIcon) => (
+                                        <li onClick={() => SetIconFn(comIcon.icon)} className='block px-4 py-4 border-b' key={comIcon.id}>
+                                            {comIcon.icon}
+                                        </li>
+                                    ))
+                                }
+
+
+                            </ul>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default DropDownBtn
